4b02043845443529dfd0b1ae05722efb6cab614e
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-10-12 11:43:54 +0530

    Add files via upload

---
+++ prgm.py
@@ -0,0 +1,112 @@
+# day_number=int(input("Enter a number from 1 to 7:"))
+# days_week=["sunday","Monday","tuesday","wednesday","thursday","friday","saturday"]
+# if 1 <= day_number <= 7:
+#     day_name=days_week[day_number]
+#     # day_name.format()
+#     print("the day correspondes to ",day_number," is" ,day_name)
+    
+# else:
+#     print("invalid input.")
+
+
+
+# month_num=int(input("Enter a number from 1 to 12:"))
+# months=["jan","feb","mar","april","may","june","july","aug","sept","oct","nov","dec"]
+# if 1 <= month_num <= 12:
+#     month_name=months[month_num -1]
+#     #month_name.format()
+#     if month_num in [1,3,5,7,9,12]:
+#         days=31
+#     elif month_num == 2:
+#         days= "28" or "29"
+#     else:
+#         days=30
+#         #display the result
+#         print(month_name,"has",days)
+# else:
+#         print("invalid input")
+
+
+# num_days=int(input("enter the number of days(28,30,31):"))
+# month_days("jan":31,"feb":28,"march":31,"april":30,"may":31,"june":30,"july":31,"aug":31,"sept":30,"oct":31,"nov":30,"dec":31)
+# month_days=month_days[num_days]
+# #month_days.format()
+# if num_days in[28,30,31]:
+#     month=[month_days]
+#     #display the result
+#     print(num_days,"is",month)
+# else:
+#     print("invalid input")
+
+
+
+# age=int(input("enter your age:"))
+# if age>=18:
+#     print("you are eligible for voting")
+# else:
+#     print("you are not eligible for voting")
+
+
+
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the second number:"))
+# op=(input("enter the operators (+,-,*,/):"))
+# if op == "+":
+#   sum= num1+num2
+#   print(sum)
+# elif op == "-":
+#     sum= num1-num2
+#     print(sum)
+# elif op =="*":
+#    sum= num1*num2
+#    print(sum)
+# elif op=="/":
+#    sum= num1/num2
+#    print(sum)
+# else:
+#    print("invalid operator")
+
+
+# num=int(input("enter the number:"))
+# if num<40:
+#     print("failed")
+# elif num>=40 and num<55:
+#     print("fair")
+# elif num>=55 and num<65:
+#     print("good")
+# elif num>=65:
+#     print("excellent")
+# else:
+#     print("invalid")
+
+
+
+
+
+
+
+# side1=float(input("enter the length of the first side:"))
+# side2=float(input("enter the length of the second side:"))
+# side3=float(input("enter the length of the third side:"))
+
+# if side1==side2==side3:
+#     print("it is an equilateral triangle")
+# elif side1!=side2!=side3:
+#     print("it is isosceles trinagle")
+# else:
+#     print("it is scalene triangle")
+  
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
