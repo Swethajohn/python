59765dd9e65f6f86eec29368d7f7dcff118a049d
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-10-13 12:58:24 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,77 @@
+# i=0
+# while i<5:
+#     print(i)
+#     i=i+1
+
+
+
+
+# for i in range(5):
+#     print(i)
+
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+# for i in range(1,10):
+#     print(i)
+
+
+
+# i=1
+# while i<=10:
+#   print(i)
+#   i=i+2
+
+
+
+# i=1
+# while i<=10:
+#     print(i,i**2)
+#     i=i+1
+
+# num=int(input("enter a number:"))
+# if num>1:
+#   for i in range(2,num)
+#    if(num%i) ==0:
+#     print(num,"it is not a prime number")
+# else:
+#     print(num,"it is a prime number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
