889a3dfd73a2a7ea05528302e2352205f1041f7e
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-10-17 11:28:32 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,86 @@
+# ques reverse string 
+
+# str = input("Enter a string: ")
+# reversed_string = ""
+# i = len(str) - 1
+
+# while i>= 0:
+#     reversed_string += str[i]
+#     i-= 1
+
+# print("Reversed string:", reversed_string)
+
+
+# ques the sum of a list of integers 
+
+# list = input("Enter a list of integers separated by spaces: ").split()
+# total=0
+# i = 0
+
+# while i < len(list):
+#     total += int(list[i])
+#     i=i+1
+
+# print("Sum of integers:", total)
+
+
+# ques 
+
+# list = input("Enter a list of integers separated by spaces: ").split()
+# largest, i = int(list[0]), 1
+
+# while i < len(list):
+#     largest = max(largest, int(list[i]))
+#     i=i+1
+
+# print("Largest integer in the list:", largest)
+
+
+
+# ques 
+
+# str = input("Enter a string: ")
+# word_count=0
+# i= 0
+
+# while i < len(str):
+#     while i < len(str) and str[i].isspace():
+#         i= i+1
+
+#     if i < len(str):
+#         word_count=word_count+1
+
+#     while i< len(str) and not str[i].isspace():
+#         i=i+1
+# print("Number of words in the string:", word_count)
+
+
+#ques
+
+
+# list = input("Enter a list of strings separated by spaces: ").split()
+# new_list = []
+# i= 0
+
+# while i < len(list):
+#     if len(list[i]) > 5:
+#         new_list.append(list[i])
+#     i=i+1
+# print("Strings with more than five characters:", new_list)
+
+
+#ques
+
+
+# list = input("Enter a list of integers separated by spaces: ").split()
+# even_list=[]
+# i=0
+
+# while i < len(list):
+#     num = int(list[i])
+#     if num % 2 == 0:
+#         even_list.append(num)
+#     i=i+1
+# print("Even integers in the list:", even_list)
+
+
