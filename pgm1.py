9d31dc50a6de746ebdd7c5d9ed66837603e1c90f
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-10-19 13:56:17 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,35 @@
+# str=input("enter a string:")
+# vowels=0
+# for i in str:
+#     if(i=='a'or i=='e'or i=='i' or i=='o'or i=='u'):
+#         vowels+=1
+# print(vowels)
+
+
+
+# str="malayalam"
+# w=""
+# for i in str:
+#     w=i+w
+# if (str==w):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+# str=input("enter a string:")
+# result=""
+# i=0
+# s=str.split()
+
+# while i< len(s):
+#     a=s[i]
+#     b=a[::-1]
+#     if a==b:
+#         c=len(s[i])
+#         s[i]=c*"@"
+#     i=i+1
+# output=" ".join(s) 
+# print(str,s,output)
+
