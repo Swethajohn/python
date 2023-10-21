0960a7f13351b1527b8dd95af7519291ebee11b3
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-10-21 13:06:48 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,96 @@
+# for i in range(10):
+#   for j in range(0,i+1):
+#     print("*",end="")
+#   print()
+  
+# n=5
+# for i in range(n,0,-1):
+#   for j in range(i):
+#     print("*",end="")
+#   print()
+
+
+# n=5
+# for i in range(1,n+1):
+# #   for j in range(i):
+#     print(""*(n-1)+"*"*i)
+  
+
+# n=5
+# for i in range(4):
+#     for p in range(0,n):
+#         print(end=' ')
+#     n=n-1
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print()
+
+
+# n=5
+# for i in range(4):
+#     for p in range(0,n):
+#         print(end=' ')
+#     n=n-1
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print()
+
+
+# n=0
+# for i in range(4,0,-1):
+#     for p in range(0,n):
+#         print(end=" ")
+#     n=n+1
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+# for i in range(5):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+
+# n=3
+# num=1
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+
+
+
+# n=3
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print((j+1)*2,end=" ")
+#     print()
+
+
+# n=3
+# num=2
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(num,end=" ")
+#         num+=2
+#     print()
+
+
+# n=3
+# num=1
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(num,end=" ")
+#         num+=2
+#     print()
+
+
+# n=3
+# for i in range(1,n+1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
