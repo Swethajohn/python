150d6bc6108b8353fe6d85ec1f652f28097639e0
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-11-04 13:33:26 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,168 @@
+# def pattern1():
+#  k=1
+#  for i in range(5):
+#     for j in range(i,k+1):
+#         print('*',end="")
+#     k=k+2
+#     print()
+# pattern1()
+
+
+
+# def pattern2():
+#     for i in range(5):
+#         for j in range(1,i+1):
+#             print(j,end="")
+#         print()
+# pattern2()
+
+
+# def name(n):
+#     new=""
+#     for i in range(len(1)-1):
+#         n=1[i]
+#         new+=(n[0].upper()+'.')
+#     new+=1[-1].title()
+#     print(new)
+# n="swetha elsa"
+# print(name(n))
+
+
+# def name(n):
+#     words=n.split()
+#     name=''.join([words.name()])
+#     print(name)
+
+
+
+# def fibonacci():
+#     n=int(input("enter a number:"))
+#     a,b=0,1
+#     while a <=n:
+#         print(a,end=" ")
+#         a,b=b, a+b
+# fibonacci()
+
+
+# def pairs(l,sum):
+#     num=sum
+# l=[3,8,12,7,6,10,21,15]
+# sum=18
+# pairs=[]
+# for i in range (len(l)):
+#     for j in range(1+1,len(l)):
+#         if l[i]+l[j]==sum:
+#             pairs.append((l[i],l[j]))
+# print("pairs with the sum of 18:")
+# print(pairs)
+    
+
+# words=["apple","banana","cherry","date"]
+# pairs=[]
+# for i in range(len(words)):
+#     for j in range(i+1,len(words)):
+#         word1=words[i]
+#         word2=words[j]
+#         if any("common words"):
+#            pairs.append((word1,word2))
+# print(pairs)
+
+
+
+# l=[2,3,4,5,6]
+# pairs=[]
+# for i in range (len(l)):
+#     l1=l[i]
+#     for j in range(1+1,len(l)
+# w=["apple","banana","cherry","date","fgklkm"]
+# pairs=[]
+# for i in range(len(w)):
+#     wo=w[i]
+#     for j in range(len(w)): 
+#         wor=w[j]
+#         if wo[i] in wor[j]:
+#            pairs.append((wo,wor))):
+     
+#       l2=l[j]
+      
+#       if (l1*l2)%2==0:
+#          if (l1+l2)%2==1:
+#             pairs.append((l1,l2))
+      
+# print(pairs)
+
+
+# words=["apple","banana","cherry","date"]
+# pairs=[]
+# for i in range(len(words)):
+#     for j in range(i+1,len(words)):
+#         word1=words[i]
+#         word2=words[j]
+#         if any("common words"):
+#            pairs.append((word1,word2))
+# print(pairs)        
+
+       
+
+
+
+# w=["apple","banana","cherry","date","fgklkm"]
+# pairs=[]
+# for i in range(len(w)):
+#     wo=w[i]
+#     for j in range(len(w)): 
+#         wor=w[j]
+#         if wo[i] in wor[j]:
+#            pairs.append((wo,wor))
+# print(pairs)
+
+
+
+
+
+# def countof():
+#     str=input("enter a string:")
+#     s=str.split()
+#     count=len(s)
+#     print(count)
+# countof()
+
+
+# def longest_word():
+#     str=input("enter a string:")
+#     s=str.split()
+#     longest=""
+#     size=0
+#     for i in range(len(s)):
+#         word=s[i]
+#         if len(word)>size:
+#             longest=word
+#             size=len(word)
+#     print(longest)
+
+# longest_word()
+
+
+
+# def capitalizes():
+#     str=input("enter a string:")
+#     s=str.split()
+#     ls=[]
+#     for i in range(len(s)):
+#         word=s[i]
+#         word2=word[0].upper()+word[1:]
+#         if word==word2:
+#             ls.append(word)
+#     print(ls)
+# capitalizes()
+
+
+
+
+
+
+
+
+
+
+
