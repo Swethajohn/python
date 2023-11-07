2ec2fffd2a3d26b3c53628fcb59161818441581b
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-11-07 13:38:00 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,45 @@
+# class cal:
+#     def __init__(self,a,b):
+#         self.n=a
+#         self.m=b
+#     def add(self):
+#         self.sum=self.n + self.m
+#         print(self.sum)
+#     def sub(self):
+#         self.sub=self.n - self.m
+#         print(self.sub)
+# k=int(input("enter 1:"))
+# p=int(input("enter 2:"))
+# ob=cal(k,p)
+# ob.add()
+# ob.sub()
+# t1=int(input("enter 3:"))
+# t2=int(input("enter 4:"))
+# ob2=cal(t1,t2)
+# ob2.add()
+# ob2.sub()
+
+
+# class Bank:
+#     def __init__(self,name,accno,balance):
+#         self.account=name
+#         self.accno=accno
+#         self.balance=balance
+#     def deposit(self):
+#         amount=int(input("enter amount:"))
+#         self.balance=self.balance+amount
+#         print(self.balance,"is balance")
+#     def withdraw(self):
+#         amount=int(input("enter amount to withdraw:"))
+#         self.balance=self.balance-amount
+#         print(self.balance,"is balance")
+# name=input("enter name:")
+# accno=int(input("enter accno:"))
+# balance=int(input("enter balance:"))
+# B1=Bank(name,accno,balance)
+# B1.deposit()
+# B1.withdraw()
+     
+
+
+    
