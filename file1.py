96f7709c17b20ee7387a1288bfb5e72d316970e1
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-11-16 14:09:08 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,11 @@
+import sys
+# import os
+# sys.path.append("/home/synnefo/Desktop/swetha/exp")
+# sys.path.insert(0,os.path.abspath("/home/synnefo/Desktop/swetha/"))
+sys.path.insert(0,"/home/synnefo/Desktop/swetha/")
+from exp.exp1 import *
+
+c.area()
+
+
+                                                                      
