000a5110ed433d33a3f662be31fd97312874297f
Swethajohn <147596085+Swethajohn@users.noreply.github.com>
2023-11-23 11:39:04 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,38 @@
+# f=open("program1.py","w")
+# f.write("swetha")
+# f.close()
+# f=open("program1.py","r")
+# print(f.read())
+
+
+# 
+
+
+# while True:
+#     ch=int(input("1.create \n 2. write \n 3. append \n 4. read \n 5.break \n"))
+#     if ch==1:
+#         fname=str(input("enter file name  "))
+#         f=open(fname,'x')
+#     if ch==2:
+#         fname=str(input("enter file name  "))
+#         f=open(fname,'w')
+#         content=str(input("enter content  "))
+#         f.write(content)
+#         f.close()
+#     if ch==3:
+#         fname=str(input("enter file name  "))
+#         f=open(fname,'a')
+#         content=str(input("enter content  "))
+#         f.write(content)
+#         f.close()
+#     if ch==4:
+#         fname=str(input("enter file name  "))
+#         f=open(fname,'r')
+#         print(f.read())
+#         f.close()
+#     if ch>=5:
+#         break
+    
+    
+
+        
